0352d3f2f43010bad80a6a1d43d0d7029d66d027
Ivan5 <converse_ivan@hotmail.com>
2019-05-03 18:14:45 -0500

    Make a navbar component

--- src/App.js
+++ src/App.js
@@ -8,15 +8,22 @@ import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 
+//import components
+import Navbar from "./components/Navbar";
+
 //import react-router
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Fragment>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/rooms" component={Rooms} />
-      <Route exact path="/rooms/:slug" component={SingleRoom} />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </Fragment>
   );
 }
