4166bf203a47ecf74c2948c1c77b1bd7e4bba4d1
Ivan5 <converse_ivan@hotmail.com>
2019-05-06 15:18:27 -0500

    Make Banner component

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
     <div>
-      <h1>Hello from Error page</h1>
+      <Hero hero="">
+        <Banner title="404" subtitle="Page not found">
+          <Link to="/" className="btn-primary">
+            Return Home
+          </Link>
+        </Banner>
+      </Hero>
     </div>
   );
 };
