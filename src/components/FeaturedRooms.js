0f6b32cdbfa6df9cb2ff75fa102d3fea1a3e2b8d
Ivan5 <converse_ivan@hotmail.com>
2019-05-07 17:45:35 -0500

    Add context for data

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -4,6 +4,7 @@ import { RoomContext } from "../context";
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
+    const { featuredRooms: rooms } = this.context;
     return (
       <div>
         <h1>from FeaturedRooms</h1>
