1df615114ef8ef09cf4de167005a795eb962e79e
Ivan5 <converse_ivan@hotmail.com>
2019-05-24 16:54:50 -0500

    Working in RoomList componenet

--- src/components/RoomsContainer.js
+++ src/components/RoomsContainer.js
@@ -18,29 +18,3 @@ function RoomsContainer({ context }) {
 }
 
 export default withRoomConsumer(RoomsContainer);
-
-/*import React from "react";
-import RoomFilter from "./RoomFilter";
-import RoomList from "./RoomList";
-import { RoomConsumer } from "../context";
-import Loading from "./Loading";
-
-export default function RoomsContainer() {
-  return (
-    <RoomConsumer>
-      {value => {
-        const { loading, sortedRooms, rooms } = value;
-        if (loading) {
-          return <Loading />;
-        }
-        return (
-          <div>
-            Hello from Rooms Container
-            <RoomFilter rooms={rooms} />
-            <RoomList rooms={sortedRooms} />
-          </div>
-        );
-      }}
-    </RoomConsumer>
-  );
-}*/
