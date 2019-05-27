a358d15527cf31a777613ee536bf5c6585c35621
Ivan5 <converse_ivan@hotmail.com>
2019-05-27 16:31:45 -0500

    Add filter by number of guests

--- src/context.js
+++ src/context.js
@@ -78,9 +78,21 @@ class RoomProvider extends Component {
       pets
     } = this.state;
 
+    //all the rooms
+
     let tempRooms = [...rooms];
+    //transform values
+    capacity = parseInt(capacity);
+
+    //filter by type
+
     if (type !== "all") {
       tempRooms = tempRooms.filter(room => room.type === type);
+    }
+
+    //filter by capacity
+    if (capacity !== 1) {
+      tempRooms = tempRooms.filter(room => room.capacity >= capacity);
     }
     this.setState({
       sortedRooms: tempRooms
