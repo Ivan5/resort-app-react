3e1561d9468b5c552cc3144438984af782e7aaba
Ivan5 <converse_ivan@hotmail.com>
2019-05-27 16:50:11 -0500

    Filter by price

--- src/context.js
+++ src/context.js
@@ -83,7 +83,7 @@ class RoomProvider extends Component {
     let tempRooms = [...rooms];
     //transform values
     capacity = parseInt(capacity);
-
+    price = parseInt(price);
     //filter by type
 
     if (type !== "all") {
@@ -94,6 +94,11 @@ class RoomProvider extends Component {
     if (capacity !== 1) {
       tempRooms = tempRooms.filter(room => room.capacity >= capacity);
     }
+
+    //filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price);
+
+    //change state
     this.setState({
       sortedRooms: tempRooms
     });
